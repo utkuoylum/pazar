857cdac80054517a79a81cb98b072e4efa2887f5
Etem Utku Oylum <utkuoylum@gmail.com>
2023-09-27 22:14:29 +0200

    slider update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,22 +13,15 @@ const [slide,setSlide] = useState(0)
   ];
 
   const handleSlideIncrement = () => {
-    if (slide === data.length -1) {
-      setSlide(0)
-    } else {
-      setSlide(curSlide => curSlide + 1)
-    }
+    setSlide(slide === data.length -1 ? 0 : curSlide => curSlide + 1)
+
   }
 const handleSlideDecrement = () => {
-  if (slide === 0) {
-    setSlide(data.length - 1)
-  } else {
-    setSlide(curSlide => curSlide - 1)
-  }
+  setSlide(slide === 0 ? data.length - 1 : curSlide => curSlide - 1)
  }
 
   useEffect(() => {
-    const interval = setInterval(()=> {handleSlideIncrement()}, 8000)
+    const interval = setInterval(()=> {handleSlideIncrement()}, 7000)
 
     return () => {
       clearInterval(interval)
@@ -41,28 +34,18 @@ const handleSlideDecrement = () => {
   return (
     <div className="w-full h-auto overflow-x-hidden">
       <div className="w-screen h-sliderHeight relative">
-        <div className="h-full w-[400vw] flex">
-          {/* {data.map((item) => (
-            <img
-              src={item}
-              alt="slider image"
+        <div className="h-full w-[400vw] flex transition-transform duration-1000" style={{transform: `translateX(-${slide * 100}vw`}}>
+          {data.map(item => <img src={item} alt="slider image" 
               className="w-screen h-full object-cover"
               loading="priority"
-              key={data.indexOf(item)}
-            />
-          ))} */}
-          <img src={data[slide]} alt="" 
-              className="w-screen h-full object-cover"
-              loading="priority"
-              key={slide}/>
+              key={data.indexOf(item)}/>)}
         </div>
-        <div className="">
-          <div className="arrow-styles right-[30rem]" onClick={handleSlideIncrement}>
-            {" "}
-            <i className="bx bx-right-arrow-circle"></i>
+        <div className="absolute w-fit left-0 right-0 mx-auto flex gap-12 bottom-20 text-[5rem] text-white">
+          <div className="arrows" onClick={handleSlideDecrement}>
+            <i className="bx bx-left-arrow-circle"></i>
           </div>
-          <div className="arrow-styles left-[30rem]" onClick={handleSlideDecrement}>
-            <i className="bx bx-left-arrow-circle"></i>{" "}
+          <div className="arrows" onClick={handleSlideIncrement}>
+            <i className="bx bx-right-arrow-circle"></i>{" "}
           </div>
         </div>
       </div>
