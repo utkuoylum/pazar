c96702be4f8cac63844a7810fe6c8194beaf5bbe
Etem Utku Oylum <utkuoylum@gmail.com>
2023-10-03 08:44:31 +0200

    login page buttons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,24 @@
-
-
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="w-full flex flex-col items-center justify-center gap-10 py-20">
+      <div className="w-full flex items-center justify-center gap-10">
+        <div className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center 
+        justify-center gap-4 hover:border-blue-600 cursor-pointer duration-300">
+          <img className="w-8" src="/assets/google.png" alt="google logo" />
+          <span className="text-sm text-gray-900">Sign in with Google</span>
+        </div>
+        <button className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300">Sign Out</button>
+       
+      </div>
+      <div className="w-full flex items-center justify-center gap-10">
+        <div className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center 
+        justify-center gap-4 hover:border-blue-600 cursor-pointer duration-300">
+          <img className="w-8" src="/assets/github.png" alt="google logo" />
+          <span className="text-sm text-gray-900">Sign in with Github</span>
+        </div>
+        <button className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300">Sign Out</button>
+       
+      </div>
+    </div>
+  );
 }
